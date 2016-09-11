9f42e34e105366fee2eea112521de5e46dfa0f69
D1Nk1 <gari4ekkoryakin@gmail.com>
2016-09-11 21:20:43 +0300

    Release

---
+++ ProxyChecker/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProxyChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 0; i < richTextBox1.Lines.Count(); i++)
+            {
+                Ping pingsend = new Ping();
+                PingReply reply = pingsend.Send(richTextBox1.Lines[i]);
+                richTextBox2.AppendText("Status: " + reply.Status.ToString() + " Ping: " + reply.RoundtripTime.ToString() + "\n");
+            }
+        }
+    }
+}
